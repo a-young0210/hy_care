3669a3f6b88ad79f994f8c7e62d9d193b1948544
sunjung-Dev <sun17789@naver.com>
2023-04-26 15:16:46 +0900

    Initial commit

---
+++ src/main/java/com/example/hycare/HyCareApplication.java
@@ -0,0 +1,13 @@
+package com.example.hycare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyCareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyCareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hycare/HyCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hycare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyCareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
