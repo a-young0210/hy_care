7907200e14ac7c33ade471951f017e0f52d83d59
a-young0210 <dkdudab@hanmail.net>
2023-06-18 16:58:21 +0900

    chatGPT_modify Add GetMethod

--- src/main/java/com/example/hycare/chatGPT/ChatService.java
+++ src/main/java/com/example/hycare/chatGPT/ChatService.java
@@ -3,6 +3,9 @@ package com.example.hycare.chatGPT;
 import io.github.flashvayne.chatgpt.service.ChatgptService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
@@ -10,8 +13,11 @@ public class ChatService {
 
     private final ChatgptService chatgptService;
 
+    @PostMapping("")
     public String getChatResponse(String prompt) {
-        // ChatGPT 에게 질문
+
+        // ChatGPT에게 질문
         return chatgptService.sendMessage(prompt);
     }
+
 }
--- src/main/java/com/example/hycare/chatGPT/testController.java
+++ src/main/java/com/example/hycare/chatGPT/testController.java
@@ -1,25 +1,28 @@
 package com.example.hycare.chatGPT;
 
 import io.github.flashvayne.chatgpt.service.ChatgptService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 @RestController
 @Slf4j
-@RequestMapping("/api/chat-gpt")
+@RequestMapping("/summary")
 public class testController {
     private final ChatService chatService;
-    private final ChatgptService chatgptService;
+    public String summary;
 
-    //chat-gpt 와 간단한 채팅 서비스 소스
-    @PostMapping("")
-    public String test(@RequestBody String question) {
+    //chat-gpt API 호출
+    @GetMapping("")
+    public String summary(@RequestBody String question) {
         // ChatGPT가 답변
-        return chatService.getChatResponse(question);
+        summary = chatService.getChatResponse(question);
+        return "redirection";
     }
+
 }
