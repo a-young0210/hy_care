6aa02c313ad27d2ea48432d5f03380e93bd5b262
Choi-mina <cnm37@naver.com>
2023-11-29 01:48:08 +0900

    Member save API modify Session Setting, Remove

--- src/main/java/com/example/hycare/controller/WebController.java
+++ src/main/java/com/example/hycare/controller/WebController.java
@@ -1,15 +1,36 @@
 package com.example.hycare.controller;
 
 import ch.qos.logback.core.model.Model;
+import com.example.hycare.entity.ResultEntity;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.Banner;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 // 화면 전환 Controller
+@Slf4j
 @Controller
 public class WebController {
+    @Value("${server.host.api}")
+    private String baseUrl;
 
     @RequestMapping("/")
-    public String mainWeb(Model model) {
+    public String mainWeb(Model model, HttpServletRequest request) {
+        // 세션 유무/ loginDiv 값에 따라 적절한 화면 return
+        HttpSession session = request.getSession();
+        if (session.getAttribute("email") != null && session.getAttribute("loginDiv").equals("0")) {
+            return "home/doctorHome.html";
+        } else if (session.getAttribute("email") != null && session.getAttribute("loginDiv").equals("1")) {
+            return "home/patientHome.html";
+        }
         return "home/home.html";
     }
 
@@ -28,11 +49,32 @@ public class WebController {
         return "login/login.html";
     }
 
-    @GetMapping("/api/auth/login/complete/{email}")
-    public String loginGoogleGet(@PathVariable String email, @RequestParam String loginDiv) {
+    @RequestMapping("/api/auth/login/complete")
+    public String loginGoogleGet(@RequestParam String email, @RequestParam String loginDiv, org.springframework.ui.Model model, HttpServletRequest request) {
+
+        HttpSession session = request.getSession();
+        session.setAttribute("loginDiv", loginDiv);
+        session.setAttribute("email", email);
+
         if(loginDiv == "0" || loginDiv.equals("0")) {
-            return "home/doctorHome.html";
+            return "/home/doctorHome.html";
         }
-        return "home/patientHome.html";
+        return "/home/patientHome.html";
+    }
+
+    @GetMapping("/session-remove")
+    public String sessionRemove(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        session.invalidate();
+        log.info("session remove success");
+        return "redirect:/";
+    }
+
+    @GetMapping("/getSessionValues")
+    public ResponseEntity<Map<String, Object>> getSessionValues(HttpSession session) {
+        Map<String, Object> sessionValues = new HashMap<>();
+        sessionValues.put("loginDiv", session.getAttribute("loginDiv"));
+        sessionValues.put("email", session.getAttribute("email"));
+        return ResponseEntity.ok(sessionValues);
     }
 }
